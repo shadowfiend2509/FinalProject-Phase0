567bc5db18f1dcc87d9a37a0fb5f3e0c109c9720
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-09-01 01:13:44 +0700

    update

--- besar.js
+++ besar.js
@@ -1,63 +1,114 @@
-function dadu1(){
+function computer1(){
     const comp = Math.random()
-    if(comp<0.15) return 1 ; 
-    if(comp>0.15 && comp<0.29) return 2 ;
-    if(comp>0.29 && comp<0.41) return 3 ;
-    if(comp>0.41 && comp<0.71) return 4 ;
-    if(comp>0.71 && comp<0.89) return 5 ;
-    return 6 ;
+    if(comp<0.15) return 1;
+    if(comp>0.15 && comp<0.29) return 2;
+    if(comp>0.29 && comp<0.41) return 3;
+    if(comp>0.41 && comp<0.71) return 4;
+    if(comp>0.71 && comp<0.89) return 5;
+    return 6;
 }
-function dadu2(){
+
+// //==================================
+
+function computer2(){
     const comp1 = Math.random()
-    if(comp1<0.15) return 1 ; 
-    if(comp1>0.15 && comp1<0.29) return 2 ;
-    if(comp1>0.29 && comp1<0.55) return 3 ;
-    if(comp1>0.55 && comp1<0.69) return 4 ;
-    if(comp1>0.69 && comp1<0.89) return 5 ;
-    return 6 ;
+    if(comp1<0.15) return 1;
+    if(comp1>0.15 && comp1<0.29) return 2;
+    if(comp1>0.29 && comp1<0.55) return 3;
+    if(comp1>0.55 && comp1<0.69) return 4;
+    if(comp1>0.69 && comp1<0.89) return 5;
+    return 6;
 }
-function dadu3(){
+
+// //==================================
+
+function computer3 () {
     const comp2 = Math.random()
-    if(comp2<0.15) return 1 ; 
-    if(comp2>0.15 && comp2<0.40) return 2 ;
-    if(comp2>0.40 && comp2<0.55) return 3 ;
-    if(comp2>0.55 && comp2<0.71) return 4 ;
-    if(comp2>0.71 && comp2<0.80) return 5 ;
-    return 6 ;
+    if(comp2<0.15) return 1;
+    if(comp2>0.15 && comp2<0.40) return 2;
+    if(comp2>0.40 && comp2<0.55) return 3;
+    if(comp2>0.55 && comp2<0.71) return 4;
+    if(comp2>0.71 && comp2<0.80) return 5;
+    return 6;
 }
 
-function hasil(){
-    return dadu1()+dadu2()+dadu3();
+// //==================================
+
+function jumlahcomputer(){
+    const jumlahcom = computer1+computer2+computer3;
+    return jumlahcom
 }
 
-var parent = document.getElementById('middle');
-var panggild1 = document.getElementsByClassName('dadu1');
-var panggild2 = document.getElementsByClassName('dadu2');
-var panggild3 = document.getElementsByClassName('dadu3');
+/**
+ * ===================================
+ */
 
+const timer = setTimeout(function(){
+    location.reload();
+},3000); // 5000 milliseconds atau 5 seconds.
 
-var after = document.getElementById('after-middle');
+const aftermiddle = document.getElementById('after-middle');
 
-var child = after.children[0]
-child.addEventListener('click',function(){
-    var life =10
-    let result = hasil()
-    if(result>10){
-        alert(result+' Kamu Benar!')
+const buttonBesar = document.getElementById('button1');
+
+const buttonKecil = document.getElementById('button2');
+
+var computer1 = computer1();
+var computer2 = computer2();
+var computer3 = computer3();
+
+buttonBesar.addEventListener('click',function(){
+    let jumlahcomm = jumlahcomputer();
+    const dadupertama = document.querySelector('#middle .dadu1');
+    const dadukedua = document.querySelector('#middle .dadu2');
+    const daduketiga = document.querySelector('#middle .dadu3');    
+    dadupertama.innerHTML = computer1;
+    dadukedua.innerHTML = computer2;
+    daduketiga.innerHTML = computer3;
+    
+    var hasill = document.querySelector('#jumlah');
+    var output = '';
+    
+    if(jumlahcomm>11){
+        output = 'TOTAL: '+jumlahcomm+' | BENAR!';
     }else{
-        alert(result+' Kamu Salah!')
+        output = 'TOTAL: '+jumlahcomm+' | SALAH!';
     }
+    hasill.innerHTML = output;
+    timer;
+    var setTimer = setInterval(function(waktu) {
+        var waktu = 3;
+        waktu--;
+    })
+    buttonBesar.innerHTML = '<h7>Akan reset setelah </h7>'+setTimer+' <h7>detik</h7>';
 })
 
-
-var child2 = after.children[1]
-child2.addEventListener('click',function(){
-    var life =10
-
-    let result = hasil()
-    if(result<10){
-        alert(result+' Kamu Benar!')
+buttonKecil.addEventListener('click',function(){
+    let jumlahcomm = jumlahcomputer();
+    const dadupertama = document.querySelector('#middle .dadu1');
+    const dadukedua = document.querySelector('#middle .dadu2');
+    const daduketiga = document.querySelector('#middle .dadu3');    
+    dadupertama.innerHTML = computer1;
+    dadukedua.innerHTML = computer2;
+    daduketiga.innerHTML = computer3;
+    
+    var hasill = document.querySelector('#jumlah');
+    var output = '';
+    
+    if(jumlahcomm<11){
+        output = 'TOTAL: '+jumlahcomm+' | BENAR!';
     }else{
-        alert(result+' Kamu Salah!')
+        output = 'TOTAL: '+jumlahcomm+' | SALAH!';
     }
+    hasill.innerHTML = output;
+    timer;
+    var setTimer = setInterval(function(waktu) {
+        var waktu = 3;
+        waktu--;
+    })
+    buttonKecil.innerHTML = '<h7>Akan reset setelah </h7>'+setTimer+' <h7>detik</h7>';
 })
+
+const rule = document.getElementById('role');
+rule.innerHTML = 'Role: 3-10 = Kecil | 11-16 = Besar<br>Hati-hati angka yang di tampilkan bisa menipu!!'
+rule.style.fontSize = '19px';
